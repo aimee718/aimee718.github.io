6762b34056c88e0c74e4a3ac0b7ff6392b191607
aimee718 <leeaimee718@gmail.com>
2020-04-13 16:48:14 -0400

    WEB222
    Website

---
+++ data.js
@@ -0,0 +1,102 @@
+window.shopping = [
+  {
+    sku: 1,
+    name: 'Straight Jeans for women (Light)',
+    price: 19.99,
+    description:
+      'Blue skinny jeans for women. Available in XS/S/M/L/XL. Materials: 76% Cotton, 13% Lyocell, 6% Polyester, 5% Elastane with zip fly, 5 pocket-styling.',
+    category: 'jeans',
+    image: '../Img/jeans1.jpg',
+    type: 1
+  },
+  {
+    sku: 2,
+    name: 'Skinny Jeans for women (Light)',
+    price: 21.99,
+    description:
+      'Light Blue straight jeans for women. Available in XS/S/M/L/XL. Materials: 76% Cotton, 13% Lyocell, 6% Polyester, 5% Elastane with zip fly, 5 pocket-styling. Super skinny leg, fits well over waist.',
+    category: 'jeans',
+    image: '../Img/jeans2.jpg',
+    type: 1
+  },
+  {
+    sku: 3,
+    name: 'Boyfriend Jeans',
+    price: 24.99,
+    description:
+      ' Washed-out Blue loose-fit Jeans with custom embroidery. Available in XS/S/M/L/XL. Materials: 76% Cotton, 13% Lyocell, 6% Polyester, 5% Elastane with zip fly, 4 pocket-styling. Super skinny leg, fits well over waist.',
+    category: 'jeans',
+    image: '../Img/jeans3.jpg',
+    type: 2
+  },
+  {
+    sku: 4,
+    name: 'Distressed Jeans',
+    price: 15.99,
+    description:
+      'Straight ankle cut distressed jeans for women.Available in S/M/L. Materials: 76% Cotton, 13% Lyocell, 6% Polyester, 5% Elastane with zip fly, 4 pocket-styling.',
+    category: 'jeans',
+    image: '../Img/jeans4.jpg',
+    type: 3
+  },
+  {
+    sku: 5,
+    name: 'Stylish Jeans for kids',
+    price: 29.99,
+    description:
+      'Ligh wash loose-fit jeans for kids. Available in one size. Materials: 76% Cotton, 13% Lyocell, 6% Polyester, 5% Elastane with zip fly, 4 pocket-styling.',
+    category: 'jeans',
+    image: '../Img/jeans5.jpg',
+    type: 2
+  },
+  {
+    sku: 6,
+    name: 'Beige coat',
+    price: 99.99,
+    description:
+      'Beige coloured belt coat for women. Available in S/M/L. Wide, straight-cut coat in viscose-blend twill with a soft, brushed finish. Removable belt at the waist with side-seam pockets.',
+    category: 'jackets',
+    image: '../Img/jacket1.jpg',
+    type: 1
+  },
+  {
+    sku: 7,
+    name: 'Cape coat for women',
+    price: 299.99,
+    description:
+      'Pumpkin-coloured cape coat for women. Available in one size, relaxed fit, hits above knee. Made with cotton, polyester, wool.',
+    category: 'jackets',
+    image: '../Img/Jacket_2.jpg',
+    type: 2
+  },
+  {
+    sku: 8,
+    name: 'Black leather jacket for men',
+    price: 59.99,
+    description:
+      'Minimalistic black leather jacket for men. Available in S/M/L. Minor details include: zip for closure, long sleeves and side pockets. Content: 100% leather; Body lining: 55% cupro, 45% cotton; Sleeve lining: 55% polyester, 45% viscose; Filling: 100% polyurethane',
+    category: 'jackets',
+    image: '../Img/Jacket_3.jpg',
+    type: 2
+  },
+  {
+    sku: 9,
+    name: 'Military jacket for women',
+    price: 301.99,
+    description:
+      'military style jacket for women. Available in XXS/XS/S/M/L/XL/XXL. Loose fit,comes with side and chest pockets. Button and zip for closure.',
+    category: 'jackets',
+    image: '../Img/Jacket_4.jpg',
+    type: 3
+  },
+  {
+    sku: 10,
+    name: 'Black Leather Jacket',
+    price: 200.99,
+    description:
+      'Black motorcycle leather jacket for men. Available in S/M/L. Slim fit, cut from tanned leather with symmetric closure. Fully lined, zippered hand pockets. Content: 100% leather; Body lining: 55% cupro, 45% cotton; Sleeve lining: 55% polyester, 45% viscose; Filling: 100% polyurethane ',
+    category: 'jackets',
+    image: '../Img/Jacket5.jpg',
+    type: 2
+  }
+];
---
+++ script.js
@@ -0,0 +1,142 @@
+(function() {
+  let shopfunc = {
+    all: window.shopping,
+
+    getProduct: function(categories) {
+      //alert(categories);
+      var ret = [];
+      shopfunc.all.map(k => {
+        if (k.category === categories) {
+          ret.push({
+            sku: `${k.sku}`,
+            name: `${k.name}`,
+            price: `${k.price}`,
+            description: `${k.description}`,
+            category: `${k.category}`,
+            image: `${k.image}`,
+            type: `${k.type}`
+          });
+        }
+      });
+      //alert(JSON.stringify(ret));
+      this.createProduct(ret);
+    },
+    clearProduct: function() {
+      let Table = document.getElementById('showproduct');
+      Table.innerHTML = '';
+    },
+      createProduct: function(product){
+        this.clearProduct();
+        let table = product;
+        let html = '';
+
+        //html += '<tr>';
+        for (let i in table) {
+            // html += tableHelper.countryCodeToFlagImg(table[i].code);
+            // html += '</td><td>' + table[i].code;
+            
+
+            html += '<div class="w3-col l3 s6">';
+            html += '  <div class="w3-container card">';
+            html += '   <a href="#openModal"><img id="id_img" class="p_image" ';
+            html += '        src="'+'./Img/'+table[i].image+'"';
+            html += '                  style="width:95%"';
+            html += '      onclick="javascript:';
+            html += '      global_method.createPopUp2(';
+            html +=        table[i].sku;  
+          
+            html += '      );"></a>';
+            html += '    <p>'+ table[i].name+'<br><b>%'+table[i].price+'</b></p>';
+            html += '    <div id="bb3"></div>';
+            html += '   </div>';
+      
+            html += '</div>';
+          
+            // html += '</div>';
+            //html += '<div id="descp3" style="" value="' + table[i].description + '">' + table[i].description + '</div>';
+        }
+        //shopfunc.createPopUp2(table[i].sku)
+        //javascript:global_method.createPopUp('+table[i].sku+');
+        //#openModal
+        //display:none;
+        document.getElementById('showproduct').innerHTML = html;
+    },
+
+    createPopUp2:function(id){
+      //document.getElementById('a1').innerHTML = 'html';
+
+     
+      var ret2 =[];
+      shopfunc.all.map(k => {
+      if(k.sku === id){
+        ret2.push({
+        sku: `${k.sku}`,
+        name:`${k.name}`,
+        price:`${k.price}`,
+        description:`${k.description}`,
+        category:`${k.category}`,
+        image:`${k.image}`,
+        type:`${k.type}`
+        });//push
+      }//if
+      
+     });//map
+
+     //alert(ret2[0].name);
+
+      let html='';
+      html += '<div id="openModal" class="modalDialog">';
+          html += '    <div>';
+          html += '        <a href="#close" title="Close" class="close">X</a>';
+          html += '        <h2 id="aaa">Product Description</h2>';
+          html += '        <div style= "font-family: Verdana; font-size: 12px">Name: '+ ret2[0].name +'</br>';
+          html += '        Price: $'+ ret2[0].price +'</br>';
+          html += '        Descp.: '+ ret2[0].description +'</div></br>';
+          html += '        <img src="./Img/'+ ret2[0].image +'"';
+          html+= ' style="width:360px ; height:510px"></img>'; 
+ 
+          html += '    </div>';
+          html += '</div>';
+
+          document.getElementById('bb3').innerHTML = html;
+      //alert(id);
+      return id;
+  },
+
+    createPopUp: function(id) {
+      let html2 = '';
+      var ret2 = [];
+      shopfunc.all.map(k => {
+        if (k.sku === id) {
+          ret2.push({
+            sku: `${k.sku}`,
+            name: `${k.name}`,
+            price: `${k.price}`,
+            description: `${k.description}`,
+            category: `${k.category}`,
+            image: `${k.image}`,
+            type: `${k.type}`
+          }); //push
+        } //if
+      }); //map
+      html2 = 'SKU: #' + ret2[0].sku + '\n';
+      html2 += 'NAME: ' + ret2[0].name + '\n';
+      html2 += 'PRICE: $' + ret2[0].price + '\n';
+      html2 += 'DESC: ' + ret2[0].description + '\n';
+      //  html2 += '<img src="./w3images/jean_1.jpg"/>';
+
+      alert(html2);
+    }
+  };
+
+  function setupMenuHandlers() {
+    // window.countriesData = require('./countries-data.js');
+    //let nodemailer = require('../node_modules/nodemailer');
+    // var global_method = shopfunc;
+    window.onload = global_method = shopfunc;
+
+    // window.onload = shopfunc.getAlert2();
+  }
+  // When the page loads, setup all event handlers by calling setup function.
+  window.onload = setupMenuHandlers;
+})();
